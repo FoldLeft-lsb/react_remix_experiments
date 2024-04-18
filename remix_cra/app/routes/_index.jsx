448b0ac5653bec741ae5d46e957259245243ef31
louis boyle <lboyle@protonmail.com>
2024-04-18 17:06:54 +0100

    use loader data

--- remix_cra/app/routes/_index.jsx
+++ remix_cra/app/routes/_index.jsx
@@ -1,4 +1,9 @@
-import { Link } from "@remix-run/react";
+import { Link, json, useLoaderData } from "@remix-run/react";
+
+export async function loader() {
+  console.log("In the loader");
+  return json(await Promise.resolve("Hello from loader"))
+}
 
 export const meta = () => {
   return [
@@ -8,10 +13,14 @@ export const meta = () => {
 };
 
 export default function Index() {
+  const greeting = useLoaderData();
+  console.log(greeting);
   return (
     <div>
       <h1>Welcome to Boat</h1>
       <Link to="/demo">Demo page</Link>
+      <br />
+      {greeting}
     </div>
   );
 }
